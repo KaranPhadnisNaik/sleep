d5e567f8c6e328f8ebea05acc90f02bbb91e71a6
Kevin Kou <kevin.kou@ucla.edu>
2017-04-02 03:58:20 -0700

    Most of the stuff done

--- Wake Up Bitch/Wake Up Bitch/AlarmListViewController.swift
+++ Wake Up Bitch/Wake Up Bitch/AlarmListViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 import Foundation
+import UserNotifications
 
 
 class AlarmListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    var formatter = DateFormatter()
 
+    let center = UNUserNotificationCenter.current()
     
+    var time = DateComponents()
     var alarms: [alarm] = []
     
     @IBOutlet var tableView: UITableView!
-    @IBOutlet weak var Edit: UIButton!
+    @IBOutlet weak var profilePic: UIImageView!
 
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if (segue.identifier == "editAlarm") {
+            let svc = segue.destination as! UINavigationController
+            let nav = svc.topViewController as! NewAlarmViewController
+            if let existing = sender as? alarm {
+                // setup svc with existing
+                nav.tempAlarm = existing
+                nav.row = existing.day
+            }
+        }
+    }
+    
+    @IBAction func saveAndUnwind(sender: UIStoryboardSegue) {
+            tableView.reloadData()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,6 +47,21 @@ class AlarmListViewController: UIViewController, UITableViewDelegate, UITableVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let photo = UIImage(named: "genericPic")
+        profilePic.image = photo
+        
+        time.hour = 8
+        time.minute = 0
+        alarms = [alarm(d:0, t:time, r:0), alarm(d:1, t:time, r:0), alarm(d:2, t:time, r:0), alarm(d:3, t:time, r:0), alarm(d:4, t:time, r:0)]
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
         
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
@@ -39,20 +74,49 @@ class AlarmListViewController: UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
         
-        formatter.dateFormat = "hh:mm a"
-        cell.textLabel?.text = formatter.string(from: self.alarms[indexPath.row].time as Date)
+        if self.alarms[indexPath.row].time.hour! > 12 {
+            cell.textLabel?.text = "\(self.alarms[indexPath.row].time.hour! - 12 ?? 12)" + ":"
+        }
+        else {
+            cell.textLabel?.text = "\(self.alarms[indexPath.row].time.hour ?? 12)" + ":"
+        }
+        if self.alarms[indexPath.row].time.minute! < 10 {
+            cell.textLabel?.text = (cell.textLabel?.text!)! + "0"
+        }
+        cell.textLabel?.text = (cell.textLabel?.text!)! + "\(self.alarms[indexPath.row].time.minute ?? 0)"
+        
+        if self.alarms[indexPath.row].time.hour! > 12 {
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " PM"
+        }
+        else {
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " AM"
+        }
         
         switch(self.alarms[indexPath.row].day) {
         case 0:
             cell.textLabel?.text = (cell.textLabel?.text!)! + " Mondays"
             break
+        case 1:
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " Tuesdays"
+            break
+        case 2:
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " Wednesdays"
+            break
+        case 3:
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " Thursdays"
+            break
+        case 4:
+            cell.textLabel?.text = (cell.textLabel?.text!)! + " Fridays"
+            break
         default:
            break
         }
-        
         return cell
     }
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "editAlarm", sender: self.alarms[indexPath.row])
+    }
 
 }
 
--- Wake Up Bitch/Wake Up Bitch/NewAlarmViewController.swift
+++ Wake Up Bitch/Wake Up Bitch/NewAlarmViewController.swift
@@ -7,24 +7,71 @@
 //
 
 import UIKit
+import UserNotifications
 
 class NewAlarmViewController: UIViewController {
+    
+    var tempAlarm : alarm?
+    var time : DateComponents?
+    var day : Int?
+    var ring : Int?
+    var row : Int?
 
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var dayOfWeek: UISegmentedControl!
     @IBOutlet weak var songSelect: UISegmentedControl!
+    @IBOutlet weak var saveButton: UIButton!
     
-    @IBAction func Save(_ sender: UIButton) {
-        let time = datePicker.date
-        let day  = dayOfWeek
-        let ring = songSelect
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
         
-        //alarms.append(d:day, t:time, r:ring)
+        var dateComponents = DateComponents()
+        let calendar = Calendar(identifier: .gregorian)
+        //var components = calendar.dateComponents([.hour, .minute], from: datePicker.date)
+        
+        dateComponents.hour = calendar.dateComponents([.hour], from: datePicker.date).hour
+        dateComponents.minute = calendar.dateComponents([.minute], from: datePicker.date).minute
+        dateComponents.day = row
+        
+
+        time = dateComponents
+        day  = dayOfWeek.selectedSegmentIndex
+        ring = songSelect.selectedSegmentIndex
+        
+        var tempDC = dateComponents
+        tempDC.day = tempDC.day! + 2
+        
+        let center = UNUserNotificationCenter.current()
+        
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //let trigger = UNCalendarNotificationTrigger(dateMatching: tempDC, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Wake up bitch!!!"
+        content.body = "Or else pay $$$"
+        content.categoryIdentifier = "customIdentifier"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+        
+        tempAlarm = alarm(d:day!, t:time!, r:ring!)
+        let svc = segue.destination as! AlarmListViewController
+        svc.alarms[row!] = tempAlarm!
     }
+    
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        dayOfWeek.selectedSegmentIndex = row!
+        dayOfWeek.isUserInteractionEnabled = false
         // Do any additional setup after loading the view.
     }
 
